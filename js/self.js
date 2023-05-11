42b039cc3f90629883dd2d4d5a59eae8a5cc20cc
Puneetprakashsharma <sharmapuneetprakash07@gmail.com>
2023-05-11 22:00:00 +0530

    first commit

---
+++ js/self.js
@@ -0,0 +1,10 @@
+function mymenu(){
+    document.getElementById("menu-mob").style.display="block";
+    document.getElementById("icon-mob").style.display="block";
+    document.getElementById("icon-ham-mob").style.display="none";
+}
+function cancel(){
+    document.getElementById("menu-mob").style.display="none";
+    document.getElementById("icon-mob").style.display="none";
+    document.getElementById("icon-ham-mob").style.display="block";
+}
